60feb5d53c9a381ce5ecae00a04d84d2062fc68c
Maribelle <maribelleleongzz@gmail.com>
2020-06-04 15:16:45 -0300

    refactored code with callback

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,38 +1,22 @@
 const request = require('request');
-const axios = require('axios');
 
-const breed = process.argv.slice(2, 3);
-
-request(
-  `https://api.thecatapi.com/v1/breeds/search?q=${breed}`,
-  (err, res, body) => {
-    if (err) {
-      console.error(err);
-    } else {
-      const data = JSON.parse(body);
-      console.log(data);
-      if (data === [] || !data[0]) {
-        console.log('Breed not found!');
+const fetchBreedDescription = function (breedName, callback) {
+  request(
+    `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`,
+    (err, res, body) => {
+      if (err) {
+        callback(err, null);
       } else {
-        console.log(data[0].description);
+        const data = JSON.parse(body);
+        // console.log(data);
+        if (data === [] || !data[0]) {
+          callback(null, 'Breed not found');
+        } else {
+          callback(null, data[0].description);
+        }
       }
     }
-  }
-);
+  );
+};
 
-// axios(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`)
-//   .then((res) => {
-//     console.log(res.data);
-//     if (res.data == []) {
-//       console.log('Breed not found!');
-//     } else {
-//       console.log(res.data.description);
-//     }
-//     // res.data !== []
-//     //   ? console.log(res.data[0].description)
-//     //   : console.log('Breed not found!');
-//   })
-//   .catch((err) => {
-//     console.log(err.response);
-//     console.error(err);
-//   });
+module.exports = { fetchBreedDescription };
